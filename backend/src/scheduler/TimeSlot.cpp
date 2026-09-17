ba86a70ee66aff23e05ee3c1f516fb321ff58447
ThanhDuy <thanhduy19042020@gmail.com>
2026-09-17 14:17:39 +0700

    Create TimeSlot.cpp

---
+++ backend/src/scheduler/TimeSlot.cpp
@@ -0,0 +1,23 @@
+#include "TimeSlot.h"
+#include <iostream>
+
+TimeSlot::TimeSlot() {
+    id = 0;
+    day = "";
+    startTime = "";
+    endTime = "";
+}
+
+TimeSlot::TimeSlot(int id, string day, string startTime, string endTime) {
+    this->id = id;
+    this->day = day;
+    this->startTime = startTime;
+    this->endTime = endTime;
+}
+
+void TimeSlot::display() {
+    cout << id << " | "
+         << day << " | "
+         << startTime << " - "
+         << endTime << endl;
+}
