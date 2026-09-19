8a58aa9515dd8f386820b46c4631c50873a5015f
ThanhDuy <thanhduy19042020@gmail.com>
2026-09-19 15:56:47 +0700

    Update main.cpp

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -4,11 +4,5 @@
 #include <iostream>
 using namespace std;
 int main(){
-  AddEdge(G, 0, 1);
-  AddEdge(G, 0, 2);
-  AddEdge(G, 0, 3);
-  AddEdge(G, 1, 2);
-  AddEdge(G, 1, 4);
-  AddEdge(G, 2, 3);
-  AddEdge(G, 3, 4);
+  
 }
