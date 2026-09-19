d4d6f0aa2a9be637e8a98fd2e3654d247c443b57
ThanhDuy <thanhduy19042020@gmail.com>
2026-09-19 14:37:49 +0700

    Refactor TimeSlot class structure and constructor
    Refactor TimeSlot class to encapsulate properties and update constructor.

--- backend/src/scheduler/TimeSlot.h
+++ backend/src/scheduler/TimeSlot.h
@@ -3,14 +3,25 @@
 
 #include <string>
 using namespace std;
-class TimeSlot {
+
+class TimeSlot
+{
+private:
+    string day;        // Thứ trong tuần
+    int slotNumber;    // Số thứ tự tiết
+    string startTime;  // Giờ bắt đầu
+    string endTime;    // Giờ kết thúc
+
 public:
-    int id;
-    string day;
-    string startTime;
-    string endTime;
     TimeSlot();
-    TimeSlot(int id, string day, string startTime, string endTime);
+
+    TimeSlot(string day, int slotNumber,
+             string startTime, string endTime);
+
+    string getDay();
+    int getSlotNumber();
+    string getStartTime();
+    string getEndTime();
 };
 
 #endif
