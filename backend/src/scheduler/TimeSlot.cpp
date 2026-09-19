8a63aafa3a31c902d0d25ef53baa8b89d979efc8
ThanhDuy <thanhduy19042020@gmail.com>
2026-09-19 14:38:16 +0700

    Refactor TimeSlot constructor and add Display method

--- backend/src/scheduler/TimeSlot.cpp
+++ backend/src/scheduler/TimeSlot.cpp
@@ -1,17 +1,48 @@
 #include "TimeSlot.h"
 #include <iostream>
 
-TimeSlot::TimeSlot() {
-    id = 0;
+using namespace std;
+
+TimeSlot::TimeSlot()
+{
     day = "";
+    slotNumber = 0;
     startTime = "";
     endTime = "";
 }
 
-TimeSlot::TimeSlot(int id, string day, string startTime, string endTime) {
-    this->id = id;
+TimeSlot::TimeSlot(string day, int slotNumber,
+                   string startTime, string endTime)
+{
     this->day = day;
+    this->slotNumber = slotNumber;
     this->startTime = startTime;
     this->endTime = endTime;
 }
 
+void TimeSlot::Display()
+{
+    cout << day << " - Slot " << slotNumber
+         << " - " << startTime
+         << " -> " << endTime << endl;
+}
+
+string TimeSlot::getDay()
+{
+    return day;
+}
+
+int TimeSlot::getSlotNumber()
+{
+    return slotNumber;
+}
+
+string TimeSlot::getStartTime()
+{
+    return startTime;
+}
+
+string TimeSlot::getEndTime()
+{
+    return endTime;
+}
