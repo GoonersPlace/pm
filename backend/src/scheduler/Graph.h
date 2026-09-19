efe9274484e98bfde2ef8d8bc0fdbb1b1412e6ee
ThanhDuy <thanhduy19042020@gmail.com>
2026-09-19 15:34:28 +0700

    Add Graph.h header file for graph data structure

---
+++ backend/src/scheduler/Graph.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#define MAX 100
+
+struct Graph
+{
+    int n;
+    int A[MAX][MAX];
+};
+
+void InitGraph(Graph &G, int n);
+
+void AddEdge(Graph &G, int u, int v);
+
+void OutputGraph(Graph G);
+
+#endif
